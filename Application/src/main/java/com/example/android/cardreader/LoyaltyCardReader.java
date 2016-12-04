3221377eb84fb4c6c1197ef142266723a4e7b100
Zrinka Fiamengo <zfiame00@fesb.hr>
2016-12-04 14:39:40 +0100

    Simplified. Not like I really know what I am doing anyway

--- Application/src/main/java/com/example/android/cardreader/LoyaltyCardReader.java
+++ Application/src/main/java/com/example/android/cardreader/LoyaltyCardReader.java
@@ -17,30 +17,18 @@ package com.example.android.cardreader;
 
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
-import android.nfc.tech.IsoDep;
-
 import com.example.android.common.logger.Log;
-
-import java.io.IOException;
 import java.lang.ref.WeakReference;
-import java.util.Arrays;
-
-import static java.lang.Integer.*;
 
 /**
  * Callback class, invoked when an NFC card is scanned while the device is running in reader mode.
  *
  * Reader mode can be invoked by calling NfcAdapter
  */
+
 public class LoyaltyCardReader implements NfcAdapter.ReaderCallback {
-    private static final String TAG = "Tag";
-    // AID for our loyalty card service.
-    private static final String SAMPLE_LOYALTY_CARD_AID = "F222222222";
-    // ISO-DEP command HEADER for selecting an AID.
-    // Format: [Class | Instruction | Parameter 1 | Parameter 2]
-    private static final String SELECT_APDU_HEADER = "00A40400";
-    // "OK" status word sent in response to SELECT AID command (0x9000)
-    private static final byte[] SELECT_OK_SW = {(byte) 0x90, (byte) 0x00};
+    private static final String TAG = "LoyaltyCardReader";
+
 
     // Weak reference to prevent retain loop. mAccountCallback is responsible for exiting
     // foreground mode before it becomes invalid (e.g. during onPause() or onStop()).
@@ -54,55 +42,12 @@ public class LoyaltyCardReader implements NfcAdapter.ReaderCallback {
         mAccountCallback = new WeakReference<AccountCallback>(accountCallback);
     }
 
-    /**
-     * Callback when a new tag is discovered by the system.
-     *
-     * <p>Communication with the card should take place here.
-     *
-     * //@param tag Discovered tag
-     */
-
-    private String bytesToHexString(byte[] src) {
-        StringBuilder stringBuilder = new StringBuilder("0x");
-        if (src == null || src.length <= 0) {
-            return null;
-        }
-
-        char[] buffer = new char[2];
-        for (int i = 0; i < src.length; i++) {
-            buffer[0] = Character.forDigit((src[i] >>> 4) & 0x0F, 16);
-            buffer[1] = Character.forDigit(src[i] & 0x0F, 16);
-            System.out.println(buffer);
-            stringBuilder.append(buffer);
-        }
-
-        return stringBuilder.toString();
-    }
-
     @Override
     public void onTagDiscovered(Tag tag) {
-        String tagID = bytesToHexString(tag.getId());
-        Log.i(TAG, "New tag discovered" + tagID);
+        String tagID = ByteArrayToHexString(tag.getId());
+        Log.i(TAG, "New tag discovered: " + tagID);
     }
 
-    /**
-     * Build APDU for SELECT AID command. This command indicates which service a reader is
-     * interested in communicating with. See ISO 7816-4.
-     *
-     * @param aid Application ID (AID) to select
-     * @return APDU for SELECT AID command
-     */
-    public static byte[] BuildSelectApdu(String aid) {
-        // Format: [CLASS | INSTRUCTION | PARAMETER 1 | PARAMETER 2 | LENGTH | DATA]
-        return HexStringToByteArray(SELECT_APDU_HEADER + String.format("%02X", aid.length() / 2) + aid);
-    }
-
-    /**
-     * Utility class to convert a byte array to a hexadecimal string.
-     *
-     * @param bytes Bytes to convert
-     * @return String, containing hexadecimal representation.
-     */
     public static String ByteArrayToHexString(byte[] bytes) {
         final char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         char[] hexChars = new char[bytes.length * 2];
@@ -115,14 +60,6 @@ public class LoyaltyCardReader implements NfcAdapter.ReaderCallback {
         return new String(hexChars);
     }
 
-    /**
-     * Utility class to convert a hexadecimal string to a byte string.
-     *
-     * <p>Behavior with input strings containing non-hexadecimal characters is undefined.
-     *
-     * @param s String containing hexadecimal characters to convert
-     * @return Byte array generated from input
-     */
     public static byte[] HexStringToByteArray(String s) {
         int len = s.length();
         byte[] data = new byte[len / 2];
